16b122096f393113bf6764b400e211bea98f4095
Steven Dirkse <sdirkse@gams.com>
2019-12-04 11:04:26 -0500

    update so install of GDX library works on Mac OS

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,14 +1,21 @@
 # first, copy the GDX library
+
+# good default but not OK for Mac OS or Windows
 gdxlib <- "gdxlib/libgdxdclib64.so"
 if (WINDOWS) {
   gdxlib <- "gdxlib/gdxdclib64.dll"
+} else {
+# I do not know how to test for Mac OS
+  gdxdylib <- "gdxlib/libgdxdclib64.dylib"
+  if (file.exists(gdxdylib)) {
+    gdxlib <- gdxdylib
+  }
 }
 dest <- file.path(R_PACKAGE_DIR, paste0("bin", R_ARCH))
 dir.create(dest, recursive = TRUE, showWarnings = FALSE)
 file.copy(gdxlib, dest, overwrite = TRUE)
 
-# next, copy the package SHLIBs normally done by default
-
+# next, copy the package SHLIBs as is normally done by default
 files <- Sys.glob(paste0("*", SHLIB_EXT))
 dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
 dir.create(dest, recursive = TRUE, showWarnings = FALSE)
